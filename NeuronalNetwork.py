63c15a96e5fec2430958a7ade28ca500e22d59f3
Félix <fgravet@laposte.net>
2018-09-09 13:34:30 +0200

    Test

--- NeuronalNetwork.py
+++ NeuronalNetwork.py
@@ -10,15 +10,16 @@ dTanh = np.vectorize(lambda x : 1/np.cosh(x)**2)
 relu = np.vectorize(lambda x : 0. if x < 0 else x)
 drelu = np.vectorize(lambda x : 0. if x < 0 else 1.)
 
+
 #Vround et Vint sont des fonctions pour arrondir les outputs
 Vround = np.vectorize(round)
 Vint = np.vectorize(int)
-	
+
 
 
 class NeuralNetwork:
 	''' Cet objet est un réseau de neurone modulaire qui résoudra vos probleme petits et grand (enfin pas trop grand quand meme )'''
-	
+
 	#Fonction que l'on n'utilise pas directement dans l'apprentissage mais qui peut être utile
 	def mutate(x):
 		''' Mute la valeur de x  '''
@@ -46,7 +47,7 @@ class NeuralNetwork:
 			r[j,0] = x[j]
 		return r
 
-	
+
 	def __init__(self, input, hidden, output, learningRate, func):
 
 		''' Initialise le réseau de neuronnes aves les données renseignées par l'utilisateur
@@ -65,7 +66,7 @@ class NeuralNetwork:
 		self.weightHO = np.array([[random() for __ in range(output)] for _ in range(hidden)]) #Weight from Hidden to Output
 		self.biasHO = np.array([random() for __ in range(output)])
 		self.lr = learningRate
-		
+
 		if(func == 'sigmoid'):
 			self.activation = sigmoid
 			self.derivate = dSigmoid
@@ -75,11 +76,11 @@ class NeuralNetwork:
 		elif(func == 'relu'):
 			self.activation = relu
 			self.derivate = drelu
-	
-	
+
+
 	def train(self, inputs, targets):
 
-		''' Entraine le réseau de neuronne avec des inputs et des outputs ''' 
+		''' Entraine le réseau de neuronne avec des inputs et des outputs '''
 
 		for i in range(inputs.shape[0]):
 
@@ -87,12 +88,12 @@ class NeuralNetwork:
 			hidden_inputs = np.dot(inputs[i],self.weightIH) + self.biasIH
 			hidden_outputs = self.activation (hidden_inputs)
 
-			
+
 			#Détermine les valeurs des neurones de sorties:
 			output_inputs = np.dot(hidden_outputs,self.weightHO) + self.biasHO
 			outputs = self.activation(output_inputs)
 
-			
+
 			#Détermine l'erreur sur les neurones de sorties:
 			output_error = targets[i]-outputs
 			weightHOT = np.transpose(self.weightHO)
@@ -100,40 +101,40 @@ class NeuralNetwork:
 			#Détermine l'erreur sur les neurones cachés:
 			hidden_error = np.dot(output_error,weightHOT)
 
-			
+
 			#Corrige les poids H->O:
 			gradient_output = self.derivate(outputs)
 			gradient_output *= output_error
 			gradient_output *= self.lr
 
 			self.biasHO += gradient_output
-			
+
 			hidden_outputs_T = self.transposeCoVect(hidden_outputs)
 			deltaW_outputs = np.dot(hidden_outputs_T,[gradient_output])
 			self.weightHO += deltaW_outputs
 
-			
+
 			#Corrige les poids I->H :
 			gradient_hidden = self.derivate(hidden_outputs)
 			gradient_hidden *= hidden_error
 			gradient_hidden *= self.lr
-			
+
 			self.biasIH += gradient_hidden
-			
+
 			inputs_T = self.transposeCoVect(inputs[i])
 			deltaW_hidden = np.dot(inputs_T,[gradient_hidden])
 			self.weightIH += deltaW_hidden
-		
+
 	def query(self,inputs):
 		''' Demande les outputs à partir d'inputs '''
 		hidden_inputs = np.dot(inputs, self.weightIH) + self.biasIH
 		hidden_outputs = self.activation(hidden_inputs)
-		
+
 		output_inputs = np.dot(hidden_outputs,self.weightHO) + self.biasHO
 		outputs = self.activation(output_inputs)
-		
+
 		return(outputs)
-		
+
 	def perf(self,inputs,outputs):
 		''' Calcule les performances du réseau de neuronne avec un set de données test différentes de celles qui on servis pour l'entrainement'''
 		cpt = 0
@@ -141,7 +142,7 @@ class NeuralNetwork:
 			if ((Vint(Vround(self.query(inputs[i]))) == outputs[i]).all()):
 				cpt+=1
 		return (cpt*100)/inputs.shape[0]
-	
+
 	def saveNN(self,name):
 		print(strMat(self.weightIH))
 		f = open(name + ".nn", 'w')
@@ -154,8 +155,8 @@ class NeuralNetwork:
 		f.write(strVec(self.biasHO) + "\n")
 		f.write(str(self.lr) + "\n")
 		f.write(str(self.function) + "\n")
-		
-	
+
+
 nn = NeuralNetwork(2,2,1,0.01,"tanh")
 for i in range(20000):
 	nn.train(np.array([0,1]), np.array([0]))
